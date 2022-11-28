718a759f4cf54bbc046c5a18ecc3f7b3eb3ee081
lenysi4ka <lenysi4ka@mail.ru>
2022-11-28 22:00:26 +0300

    Задание 1 - выполнила) Задание 2 - выполнила. Дополнила полем "должность".

--- src/Human.java
+++ src/Human.java
@@ -5,11 +5,15 @@ public class Human {
     String city;
     int age;
     int currentYear = Year.now().getValue();
+    String post;
 
-    Human(String name, String city, int age) {
+    Human(String name, String city, int age, String post) {
+
         this.name = name;
         this.city = city;
         this.age = age;
-        System.out.println("Привет! Меня зовут: " + name + ". Я из города: " + city + ". Я родился в " + (currentYear - age) + " году. Будем знакомы!");
-          }
+        this.post = post;
+
+        System.out.println("Привет! Меня зовут: " + name + ". Я из города: " + city + ". Я родился в " + (currentYear - age) + " году." + " Я работаю на должности " + post + ". Будем знакомы!");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Human maksim = new Human("Максим", "Минск", 35);
-        Human ann = new Human("Аня", "Москва", 29);
-        Human kat = new Human("Катя", "Калиниград", 28);
-        Human artem = new Human("Артём" , "Москва", 27);
+        Human maksim = new Human("Максим", "Минск", 35, "бренд-менеджером");
+        Human ann = new Human("Аня", "Москва", 29, "методистом образовательных программ");
+        Human kat = new Human("Катя", "Калиниград", 28, "продакт менеджером");
+        Human artem = new Human("Артём" , "Москва", 27, "директором по развитию бизнеса");
           }
     }
